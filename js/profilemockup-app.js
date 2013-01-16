79723635bf0721af6bd7fa632ab11f70eb935c2b
Tim Otten <to-git@think.hm>
2013-01-16 04:32:36 -0500

    profilemockup-app.js - Separate presentation/data logic  * Extract findCreateUfGroupModel()  * Inline launchDesigner()

--- js/profilemockup-app.js
+++ js/profilemockup-app.js
@@ -1,24 +1,38 @@
 cj(document).ready(function($) {
+  var findCreateUfGroupModel = function(options) {
+    var ufId = $('#test-profile-id').val();
+    if (ufId) {
+      // Retrieve UF group and fields from the api
+      CRM.api('UFGroup', 'getsingle', {id: ufId, "api.UFField.get": 1}, {
+        success: function(formData) {
+          // Note: With chaining, API returns some extraneous keys that aren't part of UFGroupModel
+          var ufGroupModel = new CRM.UF.UFGroupModel(_.pick(formData, _.keys(CRM.UF.UFGroupModel.prototype.schema)));
+          ufGroupModel.getRel('ufEntityCollection').reset([
+            {entity_name: 'contact_1', entity_type: 'IndividualModel'},
+            {entity_name: 'activity_1', entity_type: 'ActivityModel'}
+          ]);
+          ufGroupModel.getRel('ufFieldCollection').reset(_.values(formData["api.UFField.get"].values));
+          options.onLoad(ufGroupModel);
+        }
+      });
+    }
+    else {
+      // Initialize new UF group
+      var ufGroupModel = new CRM.UF.UFGroupModel();
+      ufGroupModel.getRel('ufEntityCollection').reset([
+        {entity_name: 'contact_1', entity_type: 'IndividualModel'},
+        {entity_name: 'activity_1', entity_type: 'ActivityModel'}
+      ]);
+      options.onLoad(ufGroupModel);
+    }
+  }
+
   { // placeholder to keep indentation
     // FIXME we depend on this being a global singleton, mainly to facilitate vents
     CRM.designerApp = new Backbone.Marionette.Application();
     CRM.designerApp.addRegions({
       designerRegion: '#crm-designer'
     });
-
-    /**
-     * Prepare application
-     */
-    var launchDesigner = function(ufGroupModel) {
-      var designerLayout = new CRM.Designer.DesignerLayout({
-        model: ufGroupModel,
-        el: '<div class="full-height"></div>'
-      });
-      $("#crm-designer-dialog").unblock();
-      CRM.designerApp.designerRegion.show(designerLayout);
-      CRM.designerApp.vent.trigger('resize');
-      CRM.Designer.isModified = false;
-    };
 
     var undoState = false;
     var undoAlert;
@@ -43,31 +57,6 @@ cj(document).ready(function($) {
           if (undoState === false) {
             CRM.designerApp.designerRegion && CRM.designerApp.designerRegion.close && CRM.designerApp.designerRegion.close();
             $("#crm-designer-dialog").block({message: 'Loading...', theme: true});
-            var ufId = $('#test-profile-id').val();
-            if (ufId) {
-              // Retrieve UF group and fields from the api
-              CRM.api('UFGroup', 'getsingle', {id: ufId, "api.UFField.get": 1}, {
-                success: function(formData) {
-                  // Note: With chaining, API returns some extraneous keys that aren't part of UFGroupModel
-                  var ufGroupModel = new CRM.UF.UFGroupModel(_.pick(formData, _.keys(CRM.UF.UFGroupModel.prototype.schema)));
-                  ufGroupModel.getRel('ufEntityCollection').reset([
-                    {entity_name: 'contact_1', entity_type: 'IndividualModel'},
-                    {entity_name: 'activity_1', entity_type: 'ActivityModel'}
-                  ]);
-                  ufGroupModel.getRel('ufFieldCollection').reset(_.values(formData["api.UFField.get"].values));
-                  launchDesigner(ufGroupModel);
-                }
-              });
-            }
-            else {
-              // Initialize new UF group
-              var ufGroupModel = new CRM.UF.UFGroupModel();
-              ufGroupModel.getRel('ufEntityCollection').reset([
-                {entity_name: 'contact_1', entity_type: 'IndividualModel'},
-                {entity_name: 'activity_1', entity_type: 'ActivityModel'}
-              ]);
-              launchDesigner(ufGroupModel);
-            }
             $('.ui-dialog-titlebar-close').unbind('click').click(function() {
               undoAlert && undoAlert.close && undoAlert.close();
               if (CRM.Designer.isModified) {
@@ -80,6 +69,18 @@ cj(document).ready(function($) {
               }
               $("#crm-designer-dialog").dialog('close');
               return false;
+            });
+            findCreateUfGroupModel({
+              onLoad: function(ufGroupModel) {
+                var designerLayout = new CRM.Designer.DesignerLayout({
+                  model: ufGroupModel,
+                  el: '<div class="full-height"></div>'
+                });
+                $("#crm-designer-dialog").unblock();
+                CRM.designerApp.designerRegion.show(designerLayout);
+                CRM.designerApp.vent.trigger('resize');
+                CRM.Designer.isModified = false;
+              }
             });
           }
           undoState = false;
