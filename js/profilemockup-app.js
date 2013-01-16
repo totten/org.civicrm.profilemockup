ebe6975236d87fcf5b7d3d520576adcfade7a73b
Tim Otten <to-git@think.hm>
2013-01-16 06:16:40 -0500

    profilemockup-app.js - Remove dead code; add comments

--- js/profilemockup-app.js
+++ js/profilemockup-app.js
@@ -1,9 +1,12 @@
 cj(document).ready(function($) {
-  { // placeholder to keep indentation
-    // FIXME we depend on this being a global singleton, mainly to facilitate vents
-    CRM.designerApp = new Backbone.Marionette.Application();
-    CRM.designerApp.addRegions({
-      designerRegion: '#crm-designer'
-    });
-  }
+  /**
+   * FIXME we depend on this being a global singleton, mainly to facilitate vents
+   *
+   * vents:
+   * - resize: the size/position of widgets should be adjusted
+   * - ufChanged: any part of a UFGroup was changed
+   * - formOpened: a toggleable form (such as a UFFieldView or a UFGroupView) has been opened
+   */
+  CRM.designerApp = new Backbone.Marionette.Application();
+
 });
