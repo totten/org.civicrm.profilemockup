9e5ac79e64c88a8c4327d306edc2ae1899bbf8dc
Tim Otten <to-git@think.hm>
2013-01-17 02:35:00 -0500

    CRM-11682 - Display two different profile-selectors

--- js/jquery.crmprofile.js
+++ js/jquery.crmprofile.js
@@ -1,23 +1,21 @@
 (function($) {
-  $('select.crm-profile-selector').each(function() {
-    console.log('found ', this);
+  var ufGroupCollection = new CRM.UF.UFGroupCollection(CRM.initialProfileList.values);
+  $('.crm-profile-selector').each(function() {
+    // Hide the existing <SELECT> and instead construct a ProfileSelector view.
+    // Keep them synchronized.
     var select = this;
 
-    var dummyModel = new CRM.ProfileSelector.DummyModel({
-      profile_id: $(select).val()
-    });
-    $(select).on('change', function(){
-      dummyModel.set('profile_id', $(select).val());
-    });
-
     var view = new CRM.ProfileSelector.View({
-      model: dummyModel,
-      ufGroupCollection: new CRM.UF.UFGroupCollection(CRM.initialProfileList.values),
+      ufGroupId: $(select).val(),
+      ufGroupCollection: ufGroupCollection,
       ufEntities: [
         {entity_name: 'contact_1', entity_type: 'IndividualModel'},
         {entity_name: 'activity_1', entity_type: 'ActivityModel'}
       ]
     });
+    view.on('change:ufGroupId', function() {
+      $(select).val(view.getUfGroupId());
+    })
     view.render();
     $(select).after(view.el);
   });
--- js/view/crm.profile-selector.js
+++ js/view/crm.profile-selector.js
@@ -2,18 +2,36 @@
   var CRM = (window.CRM) ? (window.CRM) : (window.CRM = {});
   if (!CRM.ProfileSelector) CRM.ProfileSelector = {};
 
+  CRM.ProfileSelector.Option = Backbone.Marionette.ItemView.extend({
+    template: '#profile_selector_option_template',
+    tagName: 'option',
+    onRender: function() {
+      this.$el.attr('value', this.model.get('id'));
+    }
+  });
+
+  CRM.ProfileSelector.Select = Backbone.Marionette.CollectionView.extend({
+    tagName: 'select',
+    itemView: CRM.ProfileSelector.Option
+  });
+
   /**
    * Render a pane with 'Select/Preview/Edit/Copy/Create' functionality for profiles.
    *
    * options:
+   *  - ufGroupId: int, the default selection
    *  - ufGroupCollection: the profiles which can be selected
    *  - ufEntities: hard-coded entity list used with any new/existing forms
    *    (this may be removed when the form-runtime is updated to support hand-picking
    *    entities for each form)
    */
-  CRM.ProfileSelector.View = Backbone.Marionette.ItemView.extend({
+  CRM.ProfileSelector.View = Backbone.Marionette.Layout.extend({
     template: '#profile_selector_template',
+    regions: {
+      selectRegion: '.crm-profile-selector-select'
+    },
     events: {
+      'change .crm-profile-selector-select select': 'onChangeUfGroupId',
       'click .crm-profile-selector-preview': 'doPreview',
       'click .crm-profile-selector-edit': 'doEdit',
       'click .crm-profile-selector-copy': 'doCopy',
@@ -22,17 +40,32 @@
     /** @var Marionette.View which specifically builds on jQuery-UI's dialog */
     activeDialog: null,
     onRender: function() {
-
+      var view = new CRM.ProfileSelector.Select({
+        collection: this.options.ufGroupCollection
+      });
+      this.selectRegion.show(view);
+      this.setUfGroupId(this.options.ufGroupId);
+    },
+    onChangeUfGroupId: function(event) {
+      this.options.ufGroupId = $(event.target).val();
+      this.trigger('change:ufGroupId', this);
+    },
+    setUfGroupId: function(value) {
+      this.options.ufGroupId = value;
+      this.$('.crm-profile-selector-select select').val(value);
+    },
+    getUfGroupId: function() {
+      return this.options.ufGroupId;
     },
     doPreview: function() {
-      console.log('doPreview');
+      console.log('doPreview', this.getUfGroupId());
     },
     doEdit: function() {
-      console.log('doEdit');
+      console.log('doEdit', this.getUfGroupId());
       var profileSelectorView = this;
       var designerDialog = new CRM.Designer.DesignerDialog({
         findCreateUfGroupModel: function(options) {
-          var ufId = $('#test-profile-id').val();
+          var ufId = profileSelectorView.getUfGroupId();
           // Retrieve UF group and fields from the api
           CRM.api('UFGroup', 'getsingle', {id: ufId, "api.UFField.get": 1}, {
             success: function(formData) {
@@ -48,10 +81,10 @@
       this.setDialog(designerDialog);
     },
     doCopy: function() {
-      console.log('doCopy');
+      console.log('doCopy', this.getUfGroupId());
     },
     doCreate: function() {
-      console.log('doCreate');
+      console.log('doCreate -- ignore value');
       var profileSelectorView = this;
       var designerDialog = new CRM.Designer.DesignerDialog({
         findCreateUfGroupModel: function(options) {
