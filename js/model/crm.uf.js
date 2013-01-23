6261f387c3c2564f41d80aa905a8e8d4e69b42d2
Coleman Watts <coleman@civicrm.org>
2013-01-23 10:30:29 -0800

    Css fixes

--- js/model/crm.uf.js
+++ js/model/crm.uf.js
@@ -24,6 +24,9 @@
   });
   var DEFAULT_PHONE_TYPE_ID = PHONE_TYPES[0].val;
 
+  /**
+   * Add a help link to a form label
+   */
   function addHelp(title, options) {
     return title + ' <a href=\'javascript:CRM.help("' + title + '", ' + JSON.stringify(options) + ')\' title="' + ts('%1 Help', {1: title}) + '" class="helpicon"></a>';
   }
